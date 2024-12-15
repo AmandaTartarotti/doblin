606058ea9871c495713cc3eccc5f2feabc6db9e3
AmandaTartarotti <amandatartarotti@gmail.com>
2024-12-15 15:00:01 +0000

    Add player_last_moves -- take user input

--- game_config.pl
+++ game_config.pl
@@ -1,59 +1,88 @@
 :- use_module(library(between)).
 
+%utils
+
 clear_buffer:-
     repeat,
     get_char(C),
     C = '\n',
     !.
 
+get_number(Value) :-
+    get_char(Char),
+    char_code(Char, Code),
+    Value is Code - 48.
+
+%game-configuration
+
+player(1, player1).
+player(2, player2).
+
 welcome:-
     write('*****************\n'),
     write('    Welcome!\n'),
     write('*****************\n').
 
-menu_mode:-  
-    write('Choose a game mode:\n'),
+game_rules:-
+    write('Rules to be defined').
+
+handle_mode(1):- game_play.
+handle_mode(_):- 
+    write('Other game_modes to be defined').
+
+game_mode:-  
+    write('\nChoose a game mode:\n'),
     write('1 - Human vs. Human\n'),
     write('2 - Human vs. Computer\n'),
-    write('3 - Computer vs. Computer\n').
-
-game_rules:-
-    write('To be defined').
+    write('3 - Computer vs. Computer\n'),
+    get_number(Mode),
+    handle_mode(Mode).
 
 game_start:-
     welcome,
-    game_board.
+    game_rules,
+    game_mode.
+
+game_play:-
+    clear_buffer,
+    write('\nHey Player 1 '),
+    player_last_moves(Move),
+    format('You chose as your last move ~w \n', [Move]),
+    write('\nHey Player 1 '),
+    game_board(Width, Length),
+    format('You chose a board ~w x ~w \n', [Width, Length]).
+
+
+player_last_moves(Move):-
+    write('What will be your last 4 moves?\n'),
+    write('1 - Empyt places\n'),
+    write('2 - Joker places\n'),
+    get_number(Move).
 
 %Logica-para-pegar-o-tamanho-do-tabuleiro
-
-get_number(Value) :-
-    get_char(Char),
-    char_code(Char, Code),
-    !,
-    Value is Code - 48,
-    handle_board(Value).
 
 handle_board(Value) :-
     between(4, 8, Value),          
     !.
 handle_board(_) :-
-    write('Invalid option. Try again:\n'),
-    clear_buffer,
+    write('Invalid option. Try again.\n'),
     game_board.
 
 board_width(Width):-
-    write('\nChoose the board WIDTH:\n'),
+    write('Choose the board WIDTH:\n'),
     write('Hint: only numbers between 4-8:'),
     get_number(Width),
+    handle_board(Width),
     clear_buffer.
 
 board_length(Length):-
     write('\nChoose the board LENGTH:\n'),
     write('Hint: only numbers between 4-8:'),
     get_number(Length),
+    handle_board(Length),
     clear_buffer.
 
-game_board :-
+game_board(Width, Length) :-
+    clear_buffer,
     board_width(Width),
-    board_length(Length),
-    format('C - You chose a board ~w x ~w \n', [Width, Length]).
+    board_length(Length).
