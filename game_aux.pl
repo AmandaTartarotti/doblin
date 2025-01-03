ddce2ebffd7cab8a94ec745bba0d2017169802bf
tatianawl <tatiana.lin.wang@gmail.com>
2025-01-03 04:29:09 +0000

    value horizontais, alguns casos por exemplo -xxx- só contam como 1 potential combination, ver melhor

--- game_aux.pl
+++ game_aux.pl
@@ -16,10 +16,15 @@ display_game(game_state(_, board_size(Width, _), PlayerInfo1, PlayerInfo2, Curre
 
     nl, write('Game Boards:\n'), nl,
 
+    %evaluate_game_state(game_state(_, _, PlayerInfo1, PlayerInfo2, CurrentPlayer), Value1, Value2),
+    %format('Player 1 Value: ~w\nPlayer 2 Value: ~w\n', [Value1, Value2]),
+    evaluate_game_state(game_state(_, _, PlayerInfo1, PlayerInfo2, CurrentPlayer), Perc1, Perc2),
+    %format('Player 1 Perc1: ~w\nPlayer 2 Perc2: ~w\n', [Perc1, Perc2]),
+
     %Imprimir informações dos jogadores
     print_player_info(PlayerInfo1, Width), nl,
-    print_player_info(PlayerInfo2, Width), nl. 
-
+    print_player_info(PlayerInfo2, Width), nl.
+   
 print_player_info(player_info(Player, _, Score, Board,_), Width) :-
     format('Player ~w score: ~w\n', [Player, Score]),
     print_player_board(Board, Width).
@@ -74,9 +79,119 @@ game_over(_,_):-
     fail.
 
 %---------------------------------------------------
- 
+%---------------------------------------------------
+
+% value for Player 1 and Player 2
+%evaluate_game_state(GameState, Value1, Value2) :-
+evaluate_game_state(game_state(_, _, PlayerInfo1, PlayerInfo2, CurrentPlayer), Perc1, Perc2):-
+    write('----------- Calculating Value for Player 1 -----------'), nl,
+    value(game_state(_, _, PlayerInfo1, PlayerInfo2, CurrentPlayer), 1, Value1),  % calculate value for Player1
+    format('Player 1 Value: ~w\n', [Value1]), nl,
+
+    write('----------- Calculating Value for Player 2 -----------'), nl,
+    value(game_state(_, _, PlayerInfo1, PlayerInfo2, CurrentPlayer), 2, Value2), % calculate value for Player2
+    format('Player 2 Value: ~w\n', [Value2]), nl,
+    format('Value1: ~w Value2: ~w\n', [Value1, Value2]),nl,
+
+    TotalValue is Value1 + Value2,
+    handle_percentages(Value1, Value2, TotalValue, Perc1, Perc2).
+
+handle_percentages(_, _, 0.0, 0, 0) :- % se TotalValue é zero, a percentagem fica a 0
+    write('-------------------------------------------------'),nl,
+    write('Player 1 Percentage em ganhar: 0%\n'),
+    write('Player 2 Percentage em ganhar: 0%\n'), 
+    write('-------------------------------------------------'),nl,nl.
+
+handle_percentages(Value1, Value2, TotalValue, Perc1, Perc2) :-
+    %Perc1 is (Value1 / TotalValue) * 100,
+    %Perc2 is (Value2 / TotalValue) * 100, nl,
+    %format('Player 1 Percentage em ganhar: 2f%\n', [Perc1]),
+    %format('Player 2 Percentage em ganhar: ~2f%\n', [Perc2]).
+    Perc1 is round((Value1 / TotalValue) * 100),
+    Perc2 is round((Value2 / TotalValue) * 100),
+    write('-------------------------------------------------'),nl,
+    format('Player 1 Percentage em ganhar: ~w%\n', [Perc1]),
+    format('Player 2 Percentage em ganhar: ~w%\n', [Perc2]) ,
+    write('-------------------------------------------------'),nl,nl.
+
+    %Perc1 is (Value1 / TotalValue) * 100,
+    %Perc1 is (Value2 / TotalValue) * 100,
+
+    %format('Player 1 Percentage: ~2f%\n', [Perc1]),
+    %format('Player 2 Percentage: ~2f%\n', [Perc2]),
+    %write('SAI DO evaluate_game_state'), nl.
 
 %value(+GameState, +Player, -Value).
+%value do Player1
+
+%FALTA 
+%1- calcular nao só
+%2- Diagonais, verticais, quadrados
+%3- COLOCAR EM PERCENTAGEM OS VALUES
+
+%value(game_state(_, _, _, player_info(_, _, OpponentScore2, OpponentBoard2,_), _), 1, Value1) :-
+value(game_state(_, _, _, player_info(Player2, _, OpponentScore2, OpponentBoard2,_), _), 1, Value1) :-
+    %format('OpponentScore2: ~w\n', [OpponentScore2]),
+    %format('Player2 score: ~w\n', [OpponentScore2]),
+    %write('ENTREI NO VALUE1 '), nl,
+    % combinações possíveis (linhas com 3 elementos consecutivos ou quadrado quase completo com uma elemento em falta)
+    count_possible_combinations(OpponentBoard2, PotentialCombinations),
+    %write('SAI DO count_possible_combinations '),nl,
+    format('OpponentScore2: ~w, PotentialCombinations: ~w\n', [OpponentScore2, PotentialCombinations]),
+    % calculo do value
+    Value1 is OpponentScore2 + 0.5 * PotentialCombinations.
+
+%value do Player2
+value(game_state(_, _, player_info(_, _, OpponentScore1, OpponentBoard1,_), _, _), 2, Value2) :-
+    %format('Player1 score: ~w\n', [OpponentScore1]),
+    %write('ENTREI NO VALUE2: '),nl,
+    % combinações possíveis (linhas com 3 elementos consecutivos ou quadrado quase completo com uma elemento em falta)
+    count_possible_combinations(OpponentBoard1, PotentialCombinations),
+    format('OpponentScore1: ~w, PotentialCombinations: ~w\n', [OpponentScore1, PotentialCombinations]),
+    % calculo do value
+    Value2 is OpponentScore1 + 0.5 * PotentialCombinations.
+
+% find_possible_combinations(+Board, -Count)
+count_possible_combinations(board(_, _, Cells), Count) :-
+    % Count potential rows
+    %write('ENTREI NO count_possible_combinations: '),nl,
+    potential_lines(Cells, RowCount),
+    %write('SAI DO potential_lines: '),nl,
+    format('Potential horizontal lines: ~w\nResulting RowCount: ~w\n', [Cells, RowCount]), nl,
+    
+    % falta verticias diagonais e quadrado
+    Count is RowCount.
+
+%---------------------------------------------
+
+% potential_lines(+Board, -Count)
+potential_lines([], 0).
+potential_lines([Line | Rest], Total) :-
+    potential_line(Line, LineCount),
+    potential_lines(Rest, RestTotal),
+    Total is LineCount + RestTotal.
+
+% potential_line(+Line, -Count)
+potential_line([], 0).
+
+% caso 1
+potential_line([A, A, A, '-' | Rest], 1) :- % 3 matching elements followed by an empty space
+    A \= '-',                                % Not empty
+    potential_line(Rest, 0).
+
+% caso 2
+potential_line(['-', A, A, A | Rest], 1) :- % Empty space followed by 3 matching elements
+    A \= '-',                               
+    potential_line(Rest, 0).
+
+%potential_line([A, A, '-', A | Rest], 1) :-
+%    A \= '-',
+%    !,
+%    potential_line(Rest, 0).
+
+
+potential_line([_ | Rest], Count) :-
+    potential_line(Rest, Count).
 
 %---------------------------------------------------
 %---------------------------------------------------
